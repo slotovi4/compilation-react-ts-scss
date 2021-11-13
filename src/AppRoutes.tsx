710fa1ea34430001a8cb59d02784472423022d53
Bruhns Roman <roman.bruns@mail.ru>
2021-11-13 19:11:51 +0300

    feat: update AppRoutes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { HomeContainer } from 'src/containers';
+import { HomePage, NotFoundPage } from 'src/pages';
 
 export const AppRoutes = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<HomeContainer />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/:id" element={<HomePage />} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
 	);

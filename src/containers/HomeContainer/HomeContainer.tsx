2eb10b892863e805f4cb826e7197edb547a3ef39
Bruhns Roman <roman.bruns@mail.ru>
2021-11-13 20:23:07 +0300

    perf: update HomeContainer

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -4,22 +4,35 @@ import { connect } from 'react-redux';
 import type { TRootState, TDispatch } from 'src/redux/store';
 
 interface IProps extends TReduxProps {
+	redirectToHome: () => void;
 	id?: string;
 }
 
-const HomeContainer = ({ title, onHeaderClick, id }: IProps) => (
-	<Home
-		title={id || title}
-		onHeaderClick={onHeaderClick}
-	/>
-);
+const HomeContainer = ({
+	title,
+	redirectToHome,
+	clearTestMoselState,
+	id
+}: IProps) => {
+	const onHeaderClick = () => {
+		clearTestMoselState();
+		redirectToHome();
+	};
+
+	return (
+		<Home
+			title={id || title}
+			onHeaderClick={onHeaderClick}
+		/>
+	);
+};
 
 const mapState = (state: TRootState) => ({
 	title: state.testModel.text,
 });
 
 const mapDispatch = (dispatch: TDispatch) => ({
-	onHeaderClick: dispatch.testModel.clearTestMoselState,
+	clearTestMoselState: dispatch.testModel.clearTestMoselState,
 });
 
 export default connect(mapState, mapDispatch)(HomeContainer);

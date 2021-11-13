c235ca504b2d4e5dc48079922785c03c0e30bb28
Bruhns Roman <roman.bruns@mail.ru>
2021-11-13 20:23:23 +0300

    perf: update HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,19 @@
 import { HomeContainer } from 'src/containers';
 import { useParams } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
 	const params = useParams();
+	const navigate = useNavigate();
+
+	const redirectToHome = () => {
+		navigate('/');
+	};
 
 	return (
-		<HomeContainer id={params.id} />
+		<HomeContainer
+			redirectToHome={redirectToHome}
+			id={params.id}
+		/>
 	);
 };

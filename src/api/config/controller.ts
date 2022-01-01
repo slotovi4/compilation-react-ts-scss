ca9550eacfd735b119d4ee98429b6a7240226b57
Bruhns Roman <roman.bruns@mail.ru>
2022-01-01 15:52:03 +0300

    perf: perf Controller

--- src/api/config/controller.ts
+++ src/api/config/controller.ts
@@ -15,49 +15,33 @@ export abstract class Controller {
 	}
 
 	protected post<T>(path?: string, data?: unknown, config?: IRequestConfig) {
-		const request = () => {
-			return axiosBase.post<T>(
-				this.concatPath(path),
-				data,
-				formatRequestConfig(config)
-			);
-		};
-
-		return request();
+		return axiosBase.post<T>(
+			this.concatPath(path),
+			data,
+			formatRequestConfig(config)
+		);
 	}
 
 	protected get<T>(path?: string, config?: IRequestConfig) {
-		const request = () => {
-			return axiosBase.get<T>(
-				this.concatPath(path),
-				formatRequestConfig(config)
-			);
-		};
-
-		return request();
+		return axiosBase.get<T>(
+			this.concatPath(path),
+			formatRequestConfig(config)
+		);
 	}
 
 	protected delete<T>(path?: string, config?: IRequestConfig) {
-		const request = () => {
-			return axiosBase.delete<T>(
-				this.concatPath(path),
-				formatRequestConfig(config)
-			);
-		};
-
-		return request();
+		return axiosBase.delete<T>(
+			this.concatPath(path),
+			formatRequestConfig(config)
+		);
 	}
 
 	protected put<T>(path?: string, data?: unknown, config?: IRequestConfig) {
-		const request = () => {
-			return axiosBase.put<T>(
-				this.concatPath(path),
-				data,
-				formatRequestConfig(config)
-			);
-		};
-
-		return request();
+		return axiosBase.put<T>(
+			this.concatPath(path),
+			data,
+			formatRequestConfig(config)
+		);
 	}
 
 	private concatPath(path?: string) {

edec2f898d3459d27ed6c6c7728fa4e5d9b97b03
Bruhns Roman <roman.bruns@mail.ru>
2022-03-30 17:47:38 +0300

    feat: update Home component

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -5,7 +5,18 @@ import { render } from 'react-dom';
 describe('Test Home component', () => {
 	it('Renders without crashing', () => {
 		const app = document.createElement('div');
+		const onThemeSwitchChange = jest.fn();
+		const onLogoClick = jest.fn();
+		const sendTestRequest = jest.fn();
 
-		render(<Home theme={null} />, app);
+		render(
+			<Home
+				theme={null}
+				onThemeSwitchChange={onThemeSwitchChange}
+				onLogoClick={onLogoClick}
+				sendTestRequest={sendTestRequest}
+			/>,
+			app
+		);
 	});
 });
